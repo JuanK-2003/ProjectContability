54360cdb81a48d37fb54b2ea1c38968da678ef4a
JuanK003 <juankdev003@gmail.com>
2022-02-08 16:12:11 -0600

    Introduction

--- ProjectContability/Form1.Designer.cs
+++ ProjectContability/Form1.Designer.cs
@@ -28,23 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ingresoDeDatosToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ingresoDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.libroDiarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablaDeDiarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.libroMayorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablaDeMayorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.balanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablaDeBalanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ingresoDeDatosToolStripMenuItem1,
+            this.libroDiarioToolStripMenuItem,
+            this.libroMayorToolStripMenuItem,
+            this.balanceToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(902, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ingresoDeDatosToolStripMenuItem1
+            // 
+            this.ingresoDeDatosToolStripMenuItem1.CheckOnClick = true;
+            this.ingresoDeDatosToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ingresoDeDatosToolStripMenuItem});
+            this.ingresoDeDatosToolStripMenuItem1.Name = "ingresoDeDatosToolStripMenuItem1";
+            this.ingresoDeDatosToolStripMenuItem1.Size = new System.Drawing.Size(49, 20);
+            this.ingresoDeDatosToolStripMenuItem1.Text = "Datos";
+            // 
+            // ingresoDeDatosToolStripMenuItem
+            // 
+            this.ingresoDeDatosToolStripMenuItem.Name = "ingresoDeDatosToolStripMenuItem";
+            this.ingresoDeDatosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ingresoDeDatosToolStripMenuItem.Text = "Ingreso de Datos";
+            // 
+            // libroDiarioToolStripMenuItem
+            // 
+            this.libroDiarioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tablaDeDiarioToolStripMenuItem});
+            this.libroDiarioToolStripMenuItem.Name = "libroDiarioToolStripMenuItem";
+            this.libroDiarioToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.libroDiarioToolStripMenuItem.Text = "Libro Diario";
+            // 
+            // tablaDeDiarioToolStripMenuItem
+            // 
+            this.tablaDeDiarioToolStripMenuItem.Name = "tablaDeDiarioToolStripMenuItem";
+            this.tablaDeDiarioToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.tablaDeDiarioToolStripMenuItem.Text = "Tabla de Diario";
+            // 
+            // libroMayorToolStripMenuItem
+            // 
+            this.libroMayorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tablaDeMayorToolStripMenuItem});
+            this.libroMayorToolStripMenuItem.Name = "libroMayorToolStripMenuItem";
+            this.libroMayorToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.libroMayorToolStripMenuItem.Text = "Libro Mayor";
+            // 
+            // tablaDeMayorToolStripMenuItem
+            // 
+            this.tablaDeMayorToolStripMenuItem.Name = "tablaDeMayorToolStripMenuItem";
+            this.tablaDeMayorToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.tablaDeMayorToolStripMenuItem.Text = "Tabla de Mayor";
+            // 
+            // balanceToolStripMenuItem
+            // 
+            this.balanceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tablaDeBalanceToolStripMenuItem});
+            this.balanceToolStripMenuItem.Name = "balanceToolStripMenuItem";
+            this.balanceToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.balanceToolStripMenuItem.Text = "Balance";
+            // 
+            // tablaDeBalanceToolStripMenuItem
+            // 
+            this.tablaDeBalanceToolStripMenuItem.Name = "tablaDeBalanceToolStripMenuItem";
+            this.tablaDeBalanceToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.tablaDeBalanceToolStripMenuItem.Text = "Tabla de balance";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = global::ProjectContability.Properties.Resources.LOGO_UMES_COLOR;
+            this.pictureBox1.Location = new System.Drawing.Point(269, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 303);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Modern No. 20", 80.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.Color.White;
+            this.Label1.Location = new System.Drawing.Point(161, 369);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(611, 109);
+            this.Label1.TabIndex = 3;
+            this.Label1.Text = "Bienvenidos!";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(902, 518);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Proyecto de Contabilidad";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ingresoDeDatosToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem libroDiarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem libroMayorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem balanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ingresoDeDatosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tablaDeDiarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tablaDeMayorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tablaDeBalanceToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Label1;
     }
 }
 
