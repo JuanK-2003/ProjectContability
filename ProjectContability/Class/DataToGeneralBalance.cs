9741030eb9450738e4939520efb341b5ba1ebce5
juanp <juanp@LAPTOP-GH0QKC17>
2022-02-13 00:59:39 -0600

    - Generación de balance general tanto como para mostrar en pantalla (lista para mostrar en dataGridView) como convertido a una cadena para imprimir.
    - Sugerencias de campos adicionales para clase Data que agilizan el conteo.
    
    - Falta consolidar los cambios en el formulario de guardado de cuentas para especificar si es activo/pasivo/capital y si es corriente/no  corriente/diferido, lo cual se especifica en los comentarios contenidos en la clase Data

---
+++ ProjectContability/Class/DataToGeneralBalance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectContability.Class
+{
+    class DataToGeneralBalance
+    {
+        private string columna1, columna2, columna3;
+        private double aux;
+
+        public DataToGeneralBalance(string n1)
+        {
+            Columna1 = n1;
+            Columna2 = "";
+            Columna3 = "";
+        }
+
+        public DataToGeneralBalance(string n1, string n2, string n3)
+        {
+            Columna1 = n1;
+            Columna2 = n2;
+            Columna3 = n3;
+        }
+
+        override public string ToString()
+        {
+            return Columna1 + "\t" + Columna2 + "\t" + Columna3;
+        }
+
+        public string Columna1 { get => columna1; set => columna1 = value; }
+        public string Columna2 { 
+            get => columna2; 
+            set => columna2 = value.Length == 0? " " : "Q " + value; 
+        }
+        public string Columna3 { 
+            get => columna3; 
+            set => columna3 = value.Length == 0 ? " " : "Q " + value; 
+        }
+    }
+}
---
+++ ProjectContability/Class/GeneralBalance.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectContability.Class
+{
+    class GeneralBalance
+    {
+        public List<DataToGeneralBalance> GenerateGeneralBalance(List<Data> cuentas)
+        {
+            // recibe una lista de cuentas con sus saldos e identificacion requeridas
+
+            string[] type1Heading = { "Activo", "Pasivo", "Capital" };
+            string[] type2Heading = { "Corriente", "No corriente", "Diferido" };
+            double[] APCsum = { 0, 0, 0 };
+
+            List<DataToGeneralBalance> dataToDisplay = new List<DataToGeneralBalance>();
+
+            for (int type1idx = 0; type1idx <= 2; type1idx++) // recorre activo, pasivo y capital
+            {
+                double sum = 0;
+
+                for (int type2idx = 0; type2idx <= 2; type2idx++) // recorre corriente, no corriente, diferido
+                {
+                    List<string> AcountNames = new List<string>();
+                    List<double> AcountBalance = new List<double>();
+
+                    for (int cidx = 0; cidx < cuentas.Count; cidx++)
+                    {
+                        if ((cuentas[cidx].Type1 == type1idx && cuentas[cidx].Type2 == type2idx) || (type1idx == 2 && cuentas[cidx].Type1 == 2))
+                        {
+                            int zidx = AcountNames.IndexOf(cuentas[cidx].NameCuenta);
+
+                            if (zidx < 0) // no esta incluida
+                            {
+                                AcountNames.Add(cuentas[cidx].NameCuenta);
+                                AcountBalance.Add(0);
+                            }
+                            else AcountBalance[zidx] = AcountBalance[zidx] + cuentas[cidx].Credit - cuentas[cidx].Debit;
+                        }
+                    }
+
+                    if (AcountNames.Count > 0)
+                    {
+                        if (type1idx == 2) dataToDisplay.Add(new DataToGeneralBalance(type1Heading[type1idx]));
+                        else dataToDisplay.Add(new DataToGeneralBalance(type1Heading[type1idx] + " " + type2Heading[type2idx]));
+
+                        for (int kidx = 0; kidx < AcountNames.Count; kidx++)
+                        {
+                            if (kidx == AcountNames.Count -1) dataToDisplay.Add(new DataToGeneralBalance(AcountNames[kidx], AcountBalance[kidx] + "", AcountBalance.Sum() + ""));
+                            else dataToDisplay.Add(new DataToGeneralBalance(AcountNames[kidx], AcountBalance[kidx] + "", ""));
+                        }
+
+                        sum += AcountBalance.Sum();
+
+                        dataToDisplay.Add(new DataToGeneralBalance(""));
+                    }
+
+                    if (type1idx == 2) break;
+                }
+
+                if (sum != 0)
+                {
+                    APCsum[type1idx] = sum;
+                    dataToDisplay.Add(new DataToGeneralBalance("SUMA TOTAL " + type1Heading[type1idx], "", sum + ""));
+                    dataToDisplay.Add(new DataToGeneralBalance(""));
+                }
+            }
+
+            dataToDisplay.Add(new DataToGeneralBalance("SUMA PASIVO + CAPITAL", "", (APCsum[1] + APCsum[2]) + ""));
+            return dataToDisplay;
+        }
+
+        public string GeneralBalanceToText(List<DataToGeneralBalance> generalBalance)
+        {
+            string ret = "\tBALANCE GENERAL\t\n";
+
+            for (int idx = 0; idx < generalBalance.Count; idx++)
+            {
+                ret += generalBalance[idx].ToString() + "\n";
+            }
+
+            return ret;
+        }
+    }
+}
