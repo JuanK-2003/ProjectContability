7393e1b0112fd308c00b06309cc2f6587064ca89
JuanK003 <juankdev003@gmail.com>
2022-02-19 12:20:53 -0600

    Add button for Excel and PDF -- Correct

--- ProjectContability/Form5.Designer.cs
+++ ProjectContability/Form5.Designer.cs
@@ -32,6 +32,8 @@
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -86,17 +88,41 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Balance General\r\n";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(603, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 64);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Exportar a  PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(775, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 64);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Exportar a Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(1529, 743);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form5";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Balance";
@@ -114,5 +140,7 @@
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- ProjectContability/Form5.cs
+++ ProjectContability/Form5.cs
@@ -10,6 +10,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+
 
 namespace ProjectContability
 {
@@ -52,6 +56,129 @@ namespace ProjectContability
         {
             return
                 File.Exists(partidasFile);
+        }
+
+        protected void balanceSaldosPDF()
+        {
+            PdfPTable pdfTable = new PdfPTable(dataGridView1.ColumnCount);
+            pdfTable.DefaultCell.Padding = 3;
+            pdfTable.WidthPercentage = 30;
+            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            pdfTable.DefaultCell.BorderWidth = 1;
+
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                cell.BackgroundColor = new iTextSharp.text.BaseColor(240, 240, 240);
+                pdfTable.AddCell(cell);
+            }
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    pdfTable.AddCell(cell.Value.ToString());
+                }
+            }
+
+            string Path = "C:\\Users\\Public\\Balance De Saldos.pdf";
+            if (!Directory.Exists(Path))
+            {
+                Directory.CreateDirectory(Path);
+            }
+            using (FileStream fs = new FileStream(Path + ".pdf", FileMode.Create))
+            {
+                Document pdfDoc = new Document(PageSize.A4, 15f, 1f, 1f, 0f);
+                PdfWriter.GetInstance(pdfDoc, fs);
+                pdfDoc.Open();
+                pdfDoc.Add(pdfTable);
+                pdfDoc.Close();
+                fs.Close();
+            }
+        }
+
+        protected void balanceGeneralPDF()
+        {
+            PdfPTable pdfTable2 = new PdfPTable(dataGridView2.ColumnCount);
+            pdfTable2.DefaultCell.Padding = 3;
+            pdfTable2.WidthPercentage = 30;
+            pdfTable2.HorizontalAlignment = Element.ALIGN_LEFT;
+            pdfTable2.DefaultCell.BorderWidth = 1;
+
+            foreach (DataGridViewColumn column in dataGridView2.Columns)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                cell.BackgroundColor = new iTextSharp.text.BaseColor(240, 240, 240);
+                pdfTable2.AddCell(cell);
+            }
+
+            foreach (DataGridViewRow row in dataGridView2.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    pdfTable2.AddCell(cell.Value.ToString());
+                }
+            }
+
+            string Path = "C:\\Users\\Public\\Balance General.pdf";
+            if (!Directory.Exists(Path))
+            {
+                Directory.CreateDirectory(Path);
+            }
+            using (FileStream fs = new FileStream(Path + ".pdf", FileMode.Create))
+            {
+                Document pdfDoc = new Document(PageSize.A4, 15f, 1f, 1f, 0f);
+                PdfWriter.GetInstance(pdfDoc, fs);
+                pdfDoc.Open();
+                pdfDoc.Add(pdfTable2);
+                pdfDoc.Close();
+                fs.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.balanceSaldosPDF();
+            this.balanceGeneralPDF();
+        }
+
+        private void copyAlltoClipboard()
+        {
+            dataGridView1.SelectAll();
+            DataObject dataObj = dataGridView1.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
+
+        private void copyAlltoClipboard2()
+        {
+            dataGridView2.SelectAll();
+            DataObject dataObj = dataGridView2.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
+
+        private void excel()
+        {
+            Microsoft.Office.Interop.Excel.Application xlexcel;
+            Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+            xlexcel = new Microsoft.Office.Interop.Excel.Application();
+            xlexcel.Visible = true;
+            xlWorkBook = xlexcel.Workbooks.Add(misValue);
+            xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            Microsoft.Office.Interop.Excel.Range CR = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[1, 1];
+            CR.Select();
+            xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            copyAlltoClipboard();
+            excel();
+            copyAlltoClipboard2();
+            excel();
         }
     }
 }
